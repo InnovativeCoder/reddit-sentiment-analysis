879f4dea455f522628e9241a904436f25d763df2
InnovativeCoder <aashitsingh2010@gmail.com>
2023-02-05 08:14:28 -0500

    syntax error fixed

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-app = Flask(__name__) CORS(app)
+app = Flask(__name__) 
+CORS(app)
 from reddit_sentiment_analysis import *
 
 
